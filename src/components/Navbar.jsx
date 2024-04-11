1bb11cd3a246e95b66d091d37e9a6d6719bf673c
Abhijeet Sachan <abhijeetsachan.088@gmail.com>
2024-04-11 12:48:09 +0530

    Add tailwind.config.js

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+function Navbar(){
+    return(
+        <div>
+            <ul className='dip  tex'>
+                <li className='text-clip '></li>
+                <li></li>
+                <li></li>
+                <li></li>
+                <li></li>
+                <li></li>
+                <li></li>
+            </ul>
+        </div>
+    )
+}
+export default Navbar;
